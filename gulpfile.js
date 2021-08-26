d975f72da6be86b165854abf411d5c8ceeeeea3c
Yulia <aroslanova.y@icloud.com>
2021-08-26 16:46:12 +0700

    Создана папка dist c оптимизированными файлами проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,79 @@ const sass        = require('gulp-sass')(require('sass'));
 const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+
+// const imagemin = import('gulp-imagemin');
+// const imagemin = import("gulp-imagemin");
+// const imagemin = require('gulp-imagemin');
+// const imagemin = import('gulp-imagemin/*.cjs');
+// import imagemin from 'node_modules/gulp-imagemin/*.mjs';
+// import imagemin from 'node_modules/gulp-imagemin';
+// import gulp from 'gulp';
+// import imagemin from 'gulp-imagemin';
 
 // Static server
 gulp.task('server', function() {
+
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
+
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-          }))
+        .pipe(rename({prefix: '', suffix: '.min',}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on("change", gulp.parallel('html'));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
+});
+
+// gulp.task('images', function () {
+//     return gulp.src("src/img/**/*")
+//         .pipe(imagemin())
+//         .pipe(gulp.dest("dist/img"))
+//         .pipe(browserSync.stream());
+// });
+
+// gulp.task ('mailer', function() {
+//     return gulp.src("src/mailer/**/*")
+//         .pipe(gulp.dest("dist/mailer"));
+// });
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html'));
+
